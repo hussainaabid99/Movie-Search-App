a0d446d567ed18216ebdbad4a5375e04daecff90
Aabid <hussain.aabid2002@gmail.com>
2024-09-16 14:08:25 +0530

    Added autocomplete ui

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,33 @@
+import { useRef } from 'react';
 import './Navbar.css';
 
 function Navbar() {
+
+     const resultListRef = useRef(null);
+
 
      return (
           <div className="navbar-wrapper">
                <div>Movie Base</div>
                <div className="search-bar">
                     <input
+                         id="movie-search-input"
                          type="text"
+                         onFocus={() => {
+                              resultListRef.current.style.display = 'block';
+                         }}
+                         onBlur={() => {
+                              resultListRef.current.style.display = 'none';
+                         }}
                          placeholder="what movie are you thinking about ... "
                     />
+                    <div id='result-list' ref={resultListRef}>
+                         <div className='autocomplete-result'>result 1</div>
+                         <div className='autocomplete-result'>result 2</div>
+                         <div className='autocomplete-result'>result 3</div>
+                    </div>
                </div>
+
                <div>
                     Theme
                </div>
